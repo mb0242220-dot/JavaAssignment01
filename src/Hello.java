4e193c3331c6726949301c62fa4bc8d0589b41ea
Manibharathi <bmani1733@gmail.com>
2025-09-09 14:28:46 +0530

    The Bank Balance

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(string[] args) {
         int balance = 0; // input
 
-        if (  balance> 0)
+        if (  balance > 0)
         {
             System.out.println("possitive balance");
         }
