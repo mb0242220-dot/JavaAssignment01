437bef6e45c1972c4225b03cc47f0eb561ec746b
Manibharathi <bmani1733@gmail.com>
2025-09-09 14:26:11 +0530

    The Bank Balance Q

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+    public static void main(string[] args) {
+        int balance = 0; // input
+
+        if (  balance > 0)
+        {
+            System.out.println("possitive balance");
+        }
+        else if( balance < 0 )
+        {
+            System.out.println("over draft");
+        }
+        else
+        {
+            System.out.println("zero balance");
+        }
+
+    }
+}
--- src/hello.java
+++
@@ -1,7 +0,0 @@
-public class hello {
-    public static void main(string[] args) {
-        System.out.println("Manibharathi ");
-        System.out.println("hi hi  ");
-
-    }
-}
